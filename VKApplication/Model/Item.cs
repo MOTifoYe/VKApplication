50e003c97cf3baec8ca90758cb434bdcb92ef40c
MOTifoYe VS <goaway0137@gmail.com>
2022-05-13 03:26:33 +0700

    Добавлен "тип". Удалено "тематика"

--- VKApplication/Model/Item.cs
+++ VKApplication/Model/Item.cs
@@ -10,12 +10,11 @@ namespace VKApplication.Model
     public class Item : BaseVM
     {
         public string Name { get; set; }
+        public string Type { get; set; }
         public string Descrition { get; set; }
         public string Path { get; set; }
         public double Size { get; set; }
-        public string Tematic { get; set; }
         public DateTime UploadDate{ get; set; }
-
         public ObservableCollection<KeyWordItem> KeyWords { get; set; } = new ObservableCollection<KeyWordItem>();
     }
 }
